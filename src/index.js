c70d63c37a5d06ccd89b1ad82d03078e98bddeec
student <student@bgit.rs>
2018-11-28 17:27:09 +0100

    add front end framework

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './view/App';
 import * as serviceWorker from './serviceWorker';
 
+import "materialize-css/dist/css/materialize.min.css"
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
